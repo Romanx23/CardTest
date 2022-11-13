83571da2ce2c5bff8b4c03ed2f162519568fbd13
Роман Панченко <romanx23@yandex.ru>
2022-11-13 13:14:54 +0300

    modified CardTest

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -13,8 +13,8 @@ public class CardTest {
     @Test
     void shouldTestFullPositiveForm() {
         Selenide.open("http://localhost:9999");
-        $("[data-test-id=name] input").setValue("Смирнова-Иванова Екатерина");
-        $("[data-test-id=phone] input").setValue("+79758868855");
+        $("[data-test-id=name] input").setValue("Петров Иван");
+        $("[data-test-id=phone] input").setValue("+79758867755");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
